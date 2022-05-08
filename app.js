ed1fd1cfe3c6582eaca0d30fe765f2a78e96717f
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2022-05-08 09:05:00 +0530

    nodemon

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require('express'); 
-const path = require ('path'); 
-const cors = require('cors');
-const bodyParser = require('body-parser') //Part #1 Point 2
-const nav = require ('./src/data/nav')//Part#2 Point 6
-
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser"); //Part #1 Point 2
+const nav = require("./src/data/nav"); //Part#2 Point 6
 
 //Part#2 Point 6
 
@@ -26,42 +25,32 @@ const nav = require ('./src/data/nav')//Part#2 Point 6
 //     }
 // ]
 
-const loginRouter = require('./src/routes/loginroute');
-const signupRouter = require('./src/routes/signuproute');
-const homeRouter = require('./src/routes/homerouter'); //Part #1 Point 3
-const booksRouter = require('./src/routes/booksroute');
-const authorsRouter = require('./src/routes/authorsroute');
+const loginRouter = require("./src/routes/loginroute");
+const signupRouter = require("./src/routes/signuproute");
+const homeRouter = require("./src/routes/homerouter"); //Part #1 Point 3
+const booksRouter = require("./src/routes/booksroute");
+const authorsRouter = require("./src/routes/authorsroute");
 
-const app = new express; 
+const app = new express();
 
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(cors()); // Part#2 Point 7
 
-app.set('views','./src/views'); 
-app.set('view engine','ejs'); 
-app.use(cors())// Part#2 Point 7
-
-
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname , '/public'))); 
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.use('/login',loginRouter); 
-app.use('/signup',signupRouter); 
-app.use('/home',homeRouter); 
-app.use('/books',booksRouter); 
-app.use('/authors',authorsRouter); 
+app.use("/login", loginRouter);
+app.use("/signup", signupRouter);
+app.use("/home", homeRouter);
+app.use("/books", booksRouter);
+app.use("/authors", authorsRouter);
 
-
-
-app.get('/',function(req,res){
-
-    res.render('index',{});
-    
+app.get("/", function (req, res) {
+  res.render("index", {});
 });
 
-
-
-
-
-app.listen(process.env.PORT || 5000,()=>{
-    console.log("Server Ready on 5000"); //Part #1 Point 5
+app.listen(process.env.PORT || 5001, () => {
+  console.log("Server Ready on 5000"); //Part #1 Point 5
 });
